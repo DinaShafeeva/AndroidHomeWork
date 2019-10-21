c57b5fd9bfd157e034f62782e25a3ea3f1845215
Dina Shafeeva <di.shafeeva@yandex.ru>
2019-10-21 16:25:22 +0300

    Home Work 4

--- app/src/main/java/com/example/androidhomework/MainActivity.kt
+++ app/src/main/java/com/example/androidhomework/MainActivity.kt
@@ -1,12 +1,35 @@
 package com.example.androidhomework
 
-import android.support.v7.app.AppCompatActivity
+import android.app.Activity
 import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+import android.content.Intent
+import android.widget.Toast
+import kotlinx.android.synthetic.main.activity_main.*
 
+const val REQUEST_CODE_SHARE = 6;
 class MainActivity : AppCompatActivity() {
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        button_main_intent.setOnClickListener() { onClick2() }
+    }
+
+    fun onClick2() {
+        val textMessage = "Privki"
+        val sendIntent = Intent().apply {
+            action = Intent.ACTION_SEND
+            putExtra(Intent.EXTRA_TEXT, textMessage)
+            type = "text/plain"
+        }
+        startActivityForResult(sendIntent, REQUEST_CODE_SHARE)
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        if (requestCode == REQUEST_CODE_SHARE && resultCode == Activity.RESULT_OK) {
+            Toast.makeText(this, "Shared", Toast.LENGTH_SHORT).show()
+        } else {
+            Toast.makeText(this, "Is Failed message", Toast.LENGTH_SHORT).show()
+        }
     }
 }
---
+++ app/src/main/java/com/example/androidhomework/SecondActivity.kt
@@ -0,0 +1,16 @@
+package com.example.androidhomework
+
+import android.content.Intent
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_second.*
+
+class SecondActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_second)
+        val intent: Intent = intent
+        textView_secondActivity_text.text = intent.extras.get(Intent.EXTRA_TEXT).toString()
+    }
+}
