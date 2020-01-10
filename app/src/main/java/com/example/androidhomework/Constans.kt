fa75590d21f21c01f6d10afefd0fd886990fda63
Dina Shafeeva <di.shafeeva@yandex.ru>
2020-01-10 15:29:21 +0300

    Added constants

---
+++ app/src/main/java/com/example/androidhomework/Constans.kt
@@ -0,0 +1,17 @@
+package com.example.androidhomework
+
+class Constans {
+    companion object {
+        const val KEY_POSITION = "POSITION"
+        const val KEY_SONG = "SONG"
+        const val KEY_TITLE = "TITLE"
+        const val KEY_SINGER = "SINGER"
+        const val KEY_GENRE = "ALBUM"
+        const val KEY_MUSIC = "MUSIC"
+        const val KEY_POSTER = "POSTER"
+        const val KEY_ICON = "ICON"
+        const val NEXT_SONG_ACTION = "com.example.AndroidHomeWork.MyService.nextSong"
+        const val PAUSE_ACTION = "com.example.AndroidHomeWork.MySevice.pause"
+        const val PREV_ACTION = "com.example.AndroidHomeWork.MyService.previousSong"
+    }
+}
