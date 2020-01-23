322962e5cd6639dc69321915eae6ff91a94c3421
Dina Shafeeva <di.shafeeva@yandex.ru>
2020-01-24 00:37:47 +0300

    Улучшено представление информации о заметке

--- app/src/main/java/com/example/androidhomework/SecondActivity.kt
+++ app/src/main/java/com/example/androidhomework/SecondActivity.kt
@@ -3,10 +3,12 @@ package com.example.androidhomework
 import android.app.Activity
 import android.content.Intent
 import android.os.Bundle
+import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 import com.example.androidhomework.dao.DataBaseHandler
 import com.example.androidhomework.recycler.Note
 import kotlinx.android.synthetic.main.activity_second.*
+import java.util.*
 
 class SecondActivity : AppCompatActivity() {
     val db = DataBaseHandler(this, DATABASE_NAME, null, DATABASE_VERSION)
@@ -15,17 +17,25 @@ class SecondActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_second)
         if (intent?.extras?.getInt(KEY_NOTE) != -1) {
-            et_title.hint = intent?.extras?.getInt(KEY_NOTE)?.let { db.getNote(it)?.title }
-            et_description.hint =
-                intent?.extras?.getInt(KEY_NOTE)?.let { db.getNote(it)?.description }
-            et_date.hint = intent?.extras?.getInt(KEY_NOTE)?.let { db.getNote(it)?.date.toString() }
-            et_longitude.hint =
-                intent?.extras?.getInt(KEY_NOTE)?.let { db.getNote(it)?.longitude.toString() }
-            et_latitude.hint =
-                intent?.extras?.getInt(KEY_NOTE)?.let { db.getNote(it)?.latitude.toString() }
+            et_title.setText(
+                intent?.extras?.getInt(KEY_NOTE)?.let { db.getNote(it)?.title },
+                TextView.BufferType.EDITABLE
+            )
+            et_description.setText(intent?.extras?.getInt(KEY_NOTE)?.let {
+                db.getNote(it)?.description
+            }, TextView.BufferType.EDITABLE)
+            et_date.setText(intent?.extras?.getInt(KEY_NOTE)?.let {
+                db.getNote(it)?.date.toString()
+            }, TextView.BufferType.EDITABLE)
+            et_longitude.setText(intent?.extras?.getInt(KEY_NOTE)?.let {
+                db.getNote(it)?.longitude.toString()
+            }, TextView.BufferType.EDITABLE)
+            et_latitude.setText(intent?.extras?.getInt(KEY_NOTE)?.let {
+                db.getNote(it)?.latitude.toString()
+            }, TextView.BufferType.EDITABLE)
 
+        } else et_date.setText(Calendar.DAY_OF_YEAR.toString(), TextView.BufferType.EDITABLE)
 
-        }
         btn_save.setOnClickListener { onClick() }
 
     }
